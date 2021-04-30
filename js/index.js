8e65157355856b697c4caf42a7e140b302126995
PerMees <dppermees@gmail.com>
2021-04-30 22:21:28 +0700

    done without changetheme, pushtotop, responsive

--- js/index.js
+++ js/index.js
@@ -10,3 +10,45 @@ window.onscroll = function (event) {
     //navbarFixed.style.padding = "25px 15px";
   }
 };
+
+$(document).ready(function () {
+  $(".owl-slider").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 1,
+      },
+    },
+  });
+  $(".owl-feedback").owlCarousel({
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      992: {
+        items: 2,
+      },
+    },
+  });
+});
+
+$(".counter").countUp({
+  time: 2000,
+  delay: 10,
+});
